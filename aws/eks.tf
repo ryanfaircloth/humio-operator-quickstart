dc29af092161971f83f4b0775ebffa4f872e7acc
Ryan Faircloth <ryan@dss-i.com>
2022-05-11 08:28:39 -0400

    feat: output cluster ID for use by other plans

--- aws/eks.tf
+++ aws/eks.tf
@@ -83,3 +83,13 @@ module "eks" {
 
 }
 
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+}
+
+
+output "cluster_id" {
+  value = module.eks.cluster_id
+}
